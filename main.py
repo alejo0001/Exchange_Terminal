230a1efae19e3ee71c9f32ed02900d473ec26a6f
Alejo Espinel <alejandroes_92@hotmail.com>
2023-09-21 21:56:04 -0500

    Open SSL certificate

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import asyncio 
 import websockets
+import ssl
 
 all_clients=[]
+
+ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+ssl_context.load_cert_chain(certfile="certificate.pem", keyfile="private_key.pem")
 
 async def send_message(message:str):
     for client in all_clients:
@@ -14,22 +18,31 @@ async def new_client_connected(client_socket,path):
     while True:
         new_message = await client_socket.recv()
         print("Client send: ",new_message)
-        send_message(new_message)
+        await send_message(new_message)
 
 async def start_server():
-    print("server started")
-    await websockets.serve(new_client_connected,'localhost',8080, origins="*")
+    try:
+       print("server started")
+       await websockets.serve(new_client_connected,'localhost',8765, ssl=ssl_context)
+          
+    except Exception:
+        print("sadsadad1")
 
 if __name__ == "__main__":
     # event_loop = asyncio.get_event_loop()
     # event_loop.run_until_complete(start_server())
     # event_loop.run_forever()
 
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
+    # loop = asyncio.new_event_loop()
+    # asyncio.set_event_loop(loop)
+    # try:
+    #     asyncio.run(start_server())
+    #     loop.run_forever()
+    # except Exception:
+    #     print("sadsadad")
     try:
-        asyncio.run(start_server())
+        loop=asyncio.get_event_loop()
+        loop.run_until_complete(start_server())
         loop.run_forever()
-    except Exception:
-        print("sadsadad")
-
+    except Exception as e:
+        print("sadsadad2"+str(e))
--- ws_client.js
+++ ws_client.js
@@ -1,9 +1,24 @@
 document.addEventListener('DOMContentLoaded',function(){
-    const websocketClient = new WebSocket("ws://localhost:8080/");
+    const websocketClient = new WebSocket("ws://localhost:8765/");
 
     websocketClient.onopen = function(){
         console.log("Client connected");
         websocketClient.send("hello")
     }
 
+    // websocketClient.onmessage = function (event) {
+    //     console.log("Mensaje recibido del servidor:", event.data);
+    // };
+
+    // websocketClient.onclose = function (event) {
+    //     if (event.wasClean) {
+    //         console.log(`Conexión cerrada limpiamente, código=${event.code}, razón=${event.reason}`);
+    //     } else {
+    //         console.error("Conexión rota"); // ejemplo: el servidor se detuvo
+    //     }
+    // };
+
+    // websocketClient.onerror = function (error) {
+    //     console.error("Error en la conexión WebSocket:", error.message);
+    // };
 },false);

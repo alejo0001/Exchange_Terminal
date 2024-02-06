2cba95462d8bef3d1be7d4935e61fea2a0d66c3f
Alejandro Espinel <fredy.espinel@o365.unab.edu.co>
2024-02-06 16:29:08 -0500

    estilos

--- ws_client.js
+++ ws_client.js
@@ -1,27 +1,3 @@
-// document.addEventListener('DOMContentLoaded',function(){
-//     const websocketClient = new WebSocket("ws://localhost:8080/");
-
-//     websocketClient.onopen = function(){
-//         console.log("Client connected");
-//         websocketClient.send("hello")
-//     }
-
-//     // websocketClient.onmessage = function (event) {
-//     //     console.log("Mensaje recibido del servidor:", event.data);
-//     // };
-
-//     // websocketClient.onclose = function (event) {
-//     //     if (event.wasClean) {
-//     //         console.log(`Conexión cerrada limpiamente, código=${event.code}, razón=${event.reason}`);
-//     //     } else {
-//     //         console.error("Conexión rota"); // ejemplo: el servidor se detuvo
-//     //     }
-//     // };
-
-//     // websocketClient.onerror = function (error) {
-//     //     console.error("Error en la conexión WebSocket:", error.message);
-//     // };
-// },false);
 let token = document.getElementById("token");
 let temporalidad = document.getElementById("temporalidad");
 let porcentaje = document.getElementById("porcentaje");
@@ -51,6 +27,16 @@ function cambiarToken(){
         // Escucha mensajes del servidor
         socket.addEventListener("message", (event) => {
             let temporalidad = document.getElementById("temporalidad");
+            let porcentaje = document.getElementById("porcentaje");
+
+            if(event.data.includes("{")){
+                let jresponse = event.data.split("Mensaje del servidor: ")[0];
+                let response= JSON.parse(jresponse);
+                porcentaje.innerHTML = response.percentage;
+            }
+            
+
+            
             console.log(`Mensaje del servidor: ${event.data}`);
             temporalidad.innerHTML = event.data;
             //alert(`Mensaje del servidor: ${event.data}`)

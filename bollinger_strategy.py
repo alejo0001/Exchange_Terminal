c85f58b791c178fe5ee7848fa2482f735397c4ec
alejo0001 <41093060+alejo0001@users.noreply.github.com>
2025-02-05 22:52:51 -0500

    bollinger_strategy

---
+++ bollinger_strategy.py
@@ -0,0 +1,139 @@
+from pybit.unified_trading import HTTP
+import time
+import pandas as pd
+from decimal import Decimal, ROUND_DOWN,ROUND_FLOOR
+import math
+from config import (bybit_api_key,bybit_secret_key)
+
+symbol= "XRPUSDT"
+timeframe="5"
+usdt = 10
+
+tp_percent = 0.2
+sl_percent = 0.4
+
+client = HTTP(api_key=bybit_api_key, api_secret=bybit_secret_key,testnet = False)
+
+#Datos de la moneda, precuio y pasos:
+step = client.get_instruments_info(category="linear",symbol=symbol)
+ticksize = float(step['result']['list'][0]['priceFilter']['tickSize'])
+price_scale = int(step['result']['list'][0]['priceScale'])
+step_precission = float(step['result']['list'][0]['lotSizeFilter']["qtyStep"])
+
+def obtener_datos_historicos(symbol,interval,limite=200):
+    response= client.get_kline(symbol=symbol,interval=interval,limite=limite)
+    if "result" in response:
+        data = pd.DataFrame(response['result']['list']).astype(float)
+        data[0] = pd.to_datetime(data[0],unit='ms')
+        data.set_index(0,inplace=True)
+        data= data[::-1].reset_index(drop=True)
+        return data
+    else:
+        raise Exception("Error al obtener datos históricos: "+ str(response))
+    
+def calcular_bandas_bollinger(data,ventana=20,desviacion=2):
+    data['MA'] = data[4].rolling(window=ventana).mean()
+    data['UpperBand'] = data['MA'] + (data[4].rolling(window=ventana).std() * desviacion)
+    data['LowerBand'] = data['MA'] - (data[4].rolling(window=ventana).std() * desviacion)
+    return data.iloc[-1]
+
+def qty_precission(qty,precission):
+    qty = math.floor(qty/precission) * precission
+    return qty
+
+def qty_step(price):
+    precission = Decimal(f"{10** price_scale}")
+    tickdec=Decimal(f"{ticksize}")
+    precio_final = (Decimal(f"{price}")*precission)/precission
+    precide = precio_final.quantize(Decimal(f"{1/precission}"),rounding=ROUND_FLOOR)
+    operaciondec=(precide/tickdec).quantize(Decimal('1'),rounding=ROUND_FLOOR)*tickdec
+    result = float(operaciondec)
+
+    return result
+
+def crear_orden(symbol,side,order_type,qty):
+    pIdx = 0
+    if side == "Sell":
+        pIdx=2
+    else:
+        pIdx=1
+    response = client.place_order(category="linear",symbol=symbol,side=side,order_type=order_type,qty=qty,timeInForce="GoodTillCancel",positionIdx=pIdx)
+    print("orden creada con éxito")
+
+def establecer_stop_loss(symbol,sl,side):
+    sl = qty_step(sl)
+    pIdx = 0
+    if side == "Sell":
+        pIdx=2
+    else:
+        pIdx=1
+    order = client.set_trading_stop(category="linear",symbol=symbol,stopLoss=sl,slTriggerB="LastPrice",positionIdx=pIdx)
+    return order
+
+def establecer_take_profit(symbol,tp,side,qty):
+    price = qty_step(tp)
+    pIdx = 0
+    if side == "Sell":
+        pIdx=1
+    else:
+        pIdx=2
+    order=client.place_order(category="linear",symbol=symbol,side=side,orderType="Limit",reduceOnly=True,qty=qty,price=price,positionIdx=pIdx)
+    return order
+
+stop = False
+tipo=''
+qty=0
+
+while True:
+    try:
+        posiciones=client.get_positions(category="linear",symbol=symbol)
+        if float(posiciones['result']['list'][0]['size']) != 0:
+            print("Hay una posición abierta en: "+symbol)
+            if not stop:
+                precio_de_entrada = float(posiciones['result']['list'][0]['avgPrice'])
+                if posiciones['result']['list'][0]['side'] == 'Buy':
+                    stop_loss_price = precio_de_entrada*(1-sl_percent/100)
+                    take_profit_price = precio_de_entrada*(1+tp_percent/100)
+                    establecer_stop_loss(symbol=symbol,sl=stop_loss_price,side="Sell")
+                    establecer_take_profit(symbol,take_profit_price,"Sell",qty)
+                    print("Stop loss y take profit activado")
+                    stop = True
+                else:
+                    stop_loss_price = precio_de_entrada*(1+sl_percent/100)
+                    take_profit_price = precio_de_entrada*(1-tp_percent/100)
+                    establecer_stop_loss(symbol=symbol,sl=stop_loss_price,side="Sell")
+                    establecer_take_profit(symbol,take_profit_price,"Buy",qty)
+                    print("Stop loss y take profit activado")
+                    stop = True
+
+        else:
+            data= obtener_datos_historicos(symbol,timeframe)
+            data = calcular_bandas_bollinger(data)
+            precio=client.get_tickers(category="linear", symbol=symbol)
+            precio = float(precio['result']['list'][0]['lastPrice'])
+
+            if precio >= data['UpperBand']:
+                precission = step_precission
+                qty = usdt/precio
+                qty = qty_precission(qty,precission)
+                if qty.is_integer():
+                    qty = int(qty)
+                print("Cantidad de monedas: "+str(qty))
+                if tipo =="long" or tipo == "":
+                    crear_orden(symbol,"Sell","Market",qty)
+                    tipo = "short"
+            
+            if precio <=data['LowerBand']:
+                precission = step_precission                
+                qty = usdt/precio
+                qty = qty_precission(qty,precission)
+                if qty.is_integer():
+                    qty = int(qty)
+                print("Cantidad de monedas: "+str(qty))
+                if tipo =="short" or tipo == "":
+                    crear_orden(symbol,"Buy","Market",qty)
+                    tipo = "long"
+            
+    except Exception as e:
+        print(f"Error en el bot: {e}")
+        time.sleep(60)
